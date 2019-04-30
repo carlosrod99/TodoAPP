ed00221b045e41361fcc30c5e72fd0df5965ed3b
carlosRodriguezO <crodriguez@eipm.es>
2019-04-30 11:24:16 +0200

    all code

---
+++ src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the TodoList app v1.0.0. It is part of React crash course </p>
+      <p>
+        Video in{" "}
+        <a href="https://www.youtube.com/watch?v=sBws8MSXN7A">
+          React JS Crash Course - 2019
+        </a>
+      </p>
+    </React.Fragment>
+  );
+}
+
+export default About;
